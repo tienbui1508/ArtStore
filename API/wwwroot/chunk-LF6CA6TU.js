12540584f693408859c4bce0bc6804deb6cf2ade
Tien Bui <tienbui1508@gmail.com>
2024-10-07 14:34:38 +1030

    update delivery methods

---
+++ API/wwwroot/chunk-LF6CA6TU.js
@@ -0,0 +1,1 @@
+import{b as u}from"./chunk-5YP3M5XQ.js";import{j as o}from"./chunk-M46FHHWK.js";import{ka as t,r as e,x as n}from"./chunk-HFQC3EG6.js";var A=(m,a)=>{let r=t(u),c=t(o);return r.currentUser()?e(!0):r.getAuthState().pipe(n(i=>i.isAuthenticated?!0:(c.navigate(["/account/login"],{queryParams:{returnUrl:a.url}}),!1)))};export{A as a};
---
+++ Infrastructure/Migrations/20241007040336_UpdateDeliveryMethods.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDeliveryMethods : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "586d226d-32dd-4e6d-8d44-32ebf3f6bb43");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5a2d6f1b-b40b-4934-a838-ad00580301ed");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "93235e26-05a0-4bcd-bbce-881bae3db6cf", null, "Admin", "ADMIN" },
+                    { "eb762467-b8ce-41ad-8ee2-b87c5061d1bc", null, "Customer", "CUSTOMER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "93235e26-05a0-4bcd-bbce-881bae3db6cf");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "eb762467-b8ce-41ad-8ee2-b87c5061d1bc");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "586d226d-32dd-4e6d-8d44-32ebf3f6bb43", null, "Admin", "ADMIN" },
+                    { "5a2d6f1b-b40b-4934-a838-ad00580301ed", null, "Customer", "CUSTOMER" }
+                });
+        }
+    }
+}
